a9eed441f75eaa7b4dfefc20946f99314b22aea6
Dennis Lau <dennislau@Denniss-Laptop.fritz.box>
2024-01-24 22:34:49 +0100

    prettier

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,30 +1,32 @@
-import ShoppingGrid from '@/components/ShoppingGrid'
-import Header from '@/components/Header'
-import Image from 'next/image'
-import {getProducts} from '@/lib/products'
-import { Suspense } from 'react'
-import Loading from './loading'
-import TaylorSwiftHero from '@/components/TaylorSwiftHero'
-export const revalidate = 60
-async function Products(){
-  const products = await getProducts()
-  return <ShoppingGrid products={products}/>
+import ShoppingGrid from "@/components/ShoppingGrid";
+import Header from "@/components/Header";
+import Image from "next/image";
+import { getProducts } from "@/lib/products";
+import { Suspense } from "react";
+import Loading from "./loading";
+import TaylorSwiftHero from "@/components/TaylorSwiftHero";
+export const revalidate = 60;
+async function Products() {
+  const products = await getProducts();
+  return <ShoppingGrid products={products} />;
 }
 
 export default async function ListEvent() {
   return (
     <>
-    <main className="flex min-h-56 flex-col  justify-between bg-stone-100 bg-[url('/concert.jpg')] bg-cover bg-top bg-no-repeat h-10">
-    <h2 className="mb-4 text-4xl tracking-tight p-24 text-white dark:text-black">New Arrivals</h2>
-    </main>
-    <main className="flex flex-col justify-center p-4 items-center">
-      {/* <TaylorSwiftHero/> */}
-      <div className=" px-4 max-w-screen-lg  mt-6">
-      <Suspense fallback={<Loading/>}>
-        <Products />
-      </Suspense>
-      </div>
-  </main>
+      <main className="flex min-h-56 flex-col  justify-between bg-stone-100 bg-[url('/concert.jpg')] bg-cover bg-top bg-no-repeat h-10">
+        <h2 className="mb-4 text-4xl tracking-tight p-24 text-white dark:text-black">
+          New Arrivals
+        </h2>
+      </main>
+      <main className="flex flex-col justify-center p-4 items-center">
+        {/* <TaylorSwiftHero/> */}
+        <div className=" px-4 max-w-screen-lg  mt-6">
+          <Suspense fallback={<Loading />}>
+            <Products />
+          </Suspense>
+        </div>
+      </main>
     </>
-  )
+  );
 }
--- components/ShoppingBlock.tsx
+++ components/ShoppingBlock.tsx
@@ -1,38 +1,43 @@
-import Image from 'next/image'
-import logoImg from '@/public/logo.png'
-import Link from 'next/link'
+import Image from "next/image";
+import logoImg from "@/public/logo.png";
+import Link from "next/link";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
-import {Product} from "@/lib/types"
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Product } from "@/lib/types";
 
 export default function ProductBlock(product: Product) {
-    return (
-        <Card>
-             <div className='relative w-full h-'>
-                <Image src={product.imageurl}  alt={product.name} height={452} width={681} priority placeholder="blur" blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="/>
-                </div>
-            <CardHeader>
-                <CardTitle>{product.name}</CardTitle>
-                <CardDescription>${product.cost} </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <p>{product.description.substring(0, 250)}...</p>
-            </CardContent>
-            <CardFooter className="flex flex-row-reverse">
-                <Button asChild>
-                <Link href={`/products/${product.id}`}>
-                     View Details
-                </Link>
-                </Button>
-            </CardFooter>
-        </Card>
-    )
-  }
-  
+  return (
+    <Card>
+      <div className="relative w-full h-">
+        <Image
+          src={product.imageurl}
+          alt={product.name}
+          height={452}
+          width={681}
+          priority
+          placeholder="blur"
+          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
+        />
+      </div>
+      <CardHeader>
+        <CardTitle>{product.name}</CardTitle>
+        <CardDescription>${product.cost} </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{product.description.substring(0, 250)}...</p>
+      </CardContent>
+      <CardFooter className="flex flex-row-reverse">
+        <Button asChild>
+          <Link href={`/products/${product.id}`}>View Details</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
