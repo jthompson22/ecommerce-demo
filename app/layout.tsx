eef45c362bff7aa0899c7a2834e3c8b7e4690524
Dennis Lau <dennislau@Denniss-Laptop.fritz.box>
2024-04-01 22:11:09 +0200

    remove speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+// import { SpeedInsights } from "@vercel/speed-insights/next"
 // import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -26,7 +26,7 @@ export default function RootLayout({
         <Header/>
         {children}
         {/* <Analytics /> */}
-        <SpeedInsights />
+        {/* <SpeedInsights /> */}
       </body>
     </html>
   )
