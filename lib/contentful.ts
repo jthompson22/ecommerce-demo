d1a8e428a4be3476a618a24d3d6c2c182efa3639
Dennis Lau <dennislau@Denniss-Laptop.fritz.box>
2024-04-01 22:06:08 +0200

    test

--- lib/contentful.ts
+++ lib/contentful.ts
@@ -1,9 +1,10 @@
 const contentfulKey = process.env.CONTENTFUL_API
-
+import { unstable_noStore } from 'next/cache'
 // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token
 export async function getHeroContent(){
-  await new Promise((resolve) => setTimeout(resolve,1000))
-  let data =  await fetch(`https://cdn.contentful.com/spaces/sm28rkz980un/environments/master/entries/nUea4fv71JppXrLPmbXSs?access_token=${contentfulKey}`, { next: { revalidate: 10 } })
+  unstable_noStore() 
+  await new Promise((resolve) => setTimeout(resolve,2000))
+  let data =  await fetch(`https://cdn.contentful.com/spaces/sm28rkz980un/environments/master/entries/nUea4fv71JppXrLPmbXSs?access_token=${contentfulKey}`, {cache: 'no-store'})
   .then(response => response.json())
   .catch((err : any) => console.log(err))
   return data.fields.copy
