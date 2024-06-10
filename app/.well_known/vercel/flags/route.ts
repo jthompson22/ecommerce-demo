298392fec3bcfccc1411a235d76f7f5e780a4be6
Dennis <dennis@Denniss-MBP.ht.home>
2024-06-10 16:43:28 -0700

    added feature flags endpoint

---
+++ app/.well_known/vercel/flags/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse, type NextRequest } from 'next/server';
+import { verifyAccess, type ApiData } from '@vercel/flags';
+
+export async function GET(request: NextRequest) {
+    const access = await verifyAccess(request.headers.get('Authorization'));
+    if (!access) return NextResponse.json(null, { status: 401 });
+
+    return NextResponse.json<ApiData>({
+        definitions: {
+            free_shipping: {
+                description: 'Shows whether free shipping is available',
+                options: [
+                    { value: false, label: 'Off' },
+                    { value: true, label: 'On' },
+                ],
+            },
+        },
+    });
+}
