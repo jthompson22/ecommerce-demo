ccf11c856f3ea8a9111592b1fef772e9f1ae4240
Dennis <dennis@Denniss-MBP.ht.home>
2024-06-13 10:46:40 -0700

    added draft mode

--- lib/contentful.ts
+++ lib/contentful.ts
@@ -3,8 +3,7 @@ const contentfulKey = process.env.CONTENTFUL_API
 const contentfulPreviewKey = process.env.CONTENTFUL_PREVIEW
 import { unstable_noStore } from 'next/cache'
 
-//const { isEnabled } = draftMode()
-const isEnabled = true
+const { isEnabled } = draftMode()
 const contentUrl = isEnabled
   ? `https://preview.contentful.com/spaces/sm28rkz980un/environments/master/entries/nUea4fv71JppXrLPmbXSs?access_token=${contentfulPreviewKey}`
   : `https://cdn.contentful.com/spaces/sm28rkz980un/environments/master/entries/nUea4fv71JppXrLPmbXSs?access_token=${contentfulKey}`
@@ -12,7 +11,7 @@ const contentUrl = isEnabled
 // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token
 export async function getHeroContent() {
   unstable_noStore()
-  let data = await fetch(contentUrl, { cache: 'no-store' })
+  let data = await fetch(contentUrl, { next: { revalidate: 300 } })
     .then(response => response.json())
     .catch((err: any) => console.log(err))
   return data.fields.copy
